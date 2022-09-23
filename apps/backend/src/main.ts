75ff3194436f4c11af17b5d8974d8bdc608582bb
Dark_Knight_1999 <veronica@asar.studio>
2022-09-23 15:31:53 +0300

    add nestjs-kafka

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      }
+    }
+  })
+
   app.enableCors({ origin: '*' });
   app.setGlobalPrefix('api');
   const options = new DocumentBuilder()
