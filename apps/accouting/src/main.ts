bb7c60f7b53c92d9688147b25c67d7a13ba866e3
Dark_Knight_1999 <veronica@asar.studio>
2022-09-27 14:16:22 +0300

    add services

---
+++ apps/accouting/src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  app.connectMicroservice({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9092'],
+      }
+    }
+  })
+
+  app.enableCors({ origin: '*' });
+  app.setGlobalPrefix('api');
+  const options = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('The accouting API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3002, '0.0.0.0', () => {
+    console.log(
+      `server listening on http://localhost:3002`,
+    );
+  });
+
+}
+bootstrap();
---
+++ apps/analytics/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb://mongodb/dataBase`,
+    ),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
