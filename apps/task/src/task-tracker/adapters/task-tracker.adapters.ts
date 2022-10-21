cd0f5eb830802b99dc69dfed63f47b80112a9fee
Dark_Knight_1999 <veronica@asar.studio>
2022-10-21 17:04:52 +0300

    add schema registry

--- apps/task/src/task-tracker/adapters/task-tracker.adapters.ts
+++ apps/task/src/task-tracker/adapters/task-tracker.adapters.ts
@@ -1,17 +1,27 @@
-import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
 import {
-  TASK_TOPIC,
-  TASK_STREAM_TOPIC,
-} from 'src/common/kafka/kafka-topics';
+  Inject,
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
+import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
 export const KAKFA_CLIENT_SYMBOL = Symbol('TASK_TRACKER_SERVICE');
+
+let SchemaTaskCreatedV2 = require('../../../../schema-registry/schemas/task/created/v2.json');
+let SchemaTaskUpdatedV2 = require('../../../../schema-registry/schemas/task/updated/v2.json');
+
+let jsv = require('JSV').JSV.createEnvironment();
+
+const TASK_TOPIC = "task"
+const TASK_STREAM_TOPIC = "task-stream"
 
 export function getKafkaModuleConfig(
   clientId = 'task-tracker',
   groupId = 'task-tracker-consumer',
 ) {
   const config = {
-    name: KAKFA_CLIENT_SYMBOL,
+    name: 'TASK_TRACKER_SERVICE',
     transport: Transport.KAFKA,
     options: {
       client: {
@@ -30,58 +40,59 @@ export function getKafkaModuleConfig(
 }
 
 type TaskCompletedEvent = {
-  eventId: string,
-  eventName: string,
-  eventVersion: number,
-  eventTime: string,
-  producer: string,
+  eventId: string;
+  eventName: string;
+  eventVersion: number;
+  eventTime: string;
+  producer: string;
   payload: {
-    id: string,
-  }
-}
+    id: string;
+  };
+};
 type ReassignedEvent = {
-  eventId: string,
-  eventName: string,
-  eventVersion: number,
-  eventTime: string,
-  producer: string,
+  eventId: string;
+  eventName: string;
+  eventVersion: number;
+  eventTime: string;
+  producer: string;
   payload: {
-    id: string,
-    assignee: string,
-  }
-}
+    id: string;
+    assignee: string;
+  };
+};
 type TaskUpdatedEvent = {
-  eventId: string,
-  eventName: string,
-  eventVersion: number,
-  eventTime: string,
-  producer: string,
+  eventId: string;
+  eventName: string;
+  eventVersion: number;
+  eventTime: string;
+  producer: string;
   payload: {
-    id: string,
-    title: string,
-    description: string,
-  }
-}
+    id: string;
+    title: string;
+    jiraId: string;
+    description: string;
+  };
+};
 type TaskCreatedEvent = {
-  eventId: string,
-  eventName: string,
-  eventVersion: number,
-  eventTime: string,
-  producer: string,
+  eventId: string;
+  eventName: string;
+  eventVersion: number;
+  eventTime: string;
+  producer: string;
   payload: {
-    id: string,
-    title: string,
-    description: string,
-    assignee: string,
-  }
-}
-
+    id: string;
+    title: string;
+    jiraId: string;
+    description: string;
+    assignee: string;
+  };
+};
 
 @Injectable()
 export class TaskAdapter implements OnModuleInit {
   readonly #logger = new Logger('TASK_TRACKER_SERVICE');
 
-  constructor(@Inject(KAKFA_CLIENT_SYMBOL) readonly kafka: ClientKafka) {}
+  constructor(@Inject('TASK_TRACKER_SERVICE') readonly kafka: ClientKafka) {}
 
   async onModuleInit() {
     await this.kafka.connect();
@@ -96,11 +107,29 @@ export class TaskAdapter implements OnModuleInit {
   }
 
   async createTask(message: TaskCreatedEvent): Promise<void> {
-    this.kafka.emit(TASK_STREAM_TOPIC, message);
+
+    const result = jsv.validate(message, SchemaTaskCreatedV2);
+
+    console.log('schemwqa', result.errors);
+    if (result) {
+      this.kafka.emit(TASK_STREAM_TOPIC, message);
+
+      return;
+    }
+    throw new NotFoundException('Schema is not valid');
   }
 
   async updateTask(message: TaskUpdatedEvent): Promise<void> {
-    this.kafka.emit(TASK_STREAM_TOPIC, message);
+   console.log('')
+   const result = jsv.validate(message, SchemaTaskUpdatedV2);
+    if (result) {
+      console.log('')
+      this.kafka.emit(TASK_STREAM_TOPIC, message);
+      return;
+    } else {
+      message.eventVersion = 1;
+      this.kafka.emit(TASK_STREAM_TOPIC, message);
+    }
   }
 
   async completeTask(message: TaskCompletedEvent): Promise<void> {
